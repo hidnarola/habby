f8b1b0979b814abfbd3aadccad3fdca6ea370ad3
Hiren <hda@narola.email>
2016-10-04 16:11:00 +0530

    configuration changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,11 +50,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   | Examples:	my-controller/index	-> my_controller/index
   |		my-controller/my-method	-> my_controller/my_method
  */
-$route['default_controller'] = 'welcome';
-$route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
 
 // ------------------------------------------------------------------------
+
 $route['login'] = "user/login/index";
 $route['home'] = "user/home/index";
 $route['home/change_lang'] = "user/home/change_lang";
+
+$route['default_controller'] = 'login';
+$route['404_override'] = '';
+$route['translate_uri_dashes'] = FALSE;
