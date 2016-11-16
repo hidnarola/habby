933815e3e3398bd97eef26d9e141f801bf5d706f
Hiren <hda@narola.email>
2016-11-16 18:46:46 +0530

    design changes

--- application/views/user/partial/events/load_more_images.php
+++ application/views/user/partial/events/load_more_images.php
@@ -1,8 +1,8 @@
 <?php
     foreach ($images as $image) {
         ?>
-        <div class='col-sm-6 col-xs-12'>
-            <img src='<?php echo DEFAULT_CHAT_IMAGE_PATH . $image; ?>' class="img-responsive topi_image"/>
+        <div class='col-sm-6 col-xs-12 load_more_event_image'>
+            <img src='<?php echo DEFAULT_CHAT_IMAGE_PATH . $image; ?>' class="img-responsive"/>
         </div>
         <?php
     }
