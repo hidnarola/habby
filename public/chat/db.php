4c8b6b24723c9339a7a8811bcf70951e7dd3e101
Hiren <hda@narola.email>
2016-11-18 12:37:03 +0530

    socket server changes

--- public/chat/db.php
+++ public/chat/db.php
@@ -1,9 +1,8 @@
 <?php
 
-$conn = mysqli_connect("192.168.1.201", "habby", "6735C63zY35gOwF", "habby");
-mysqli_set_charset($conn, "utf8");
+    $conn = mysqli_connect("habby-go.c2k2g1789ryk.us-west-2.rds.amazonaws.com", "habby_go", "df098gdf790gdf7890gdf8g", "habby");
+    mysqli_set_charset($conn, "utf8");
 
-//    $conn = mysqli_connect("habby-go.c2k2g1789ryk.us-west-2.rds.amazonaws.com", "habby_go", "df098gdf790gdf7890gdf8g", "habby");
 function select($query) {
     global $conn;
     $result = mysqli_query($conn, $query);
@@ -24,7 +23,7 @@ function insert_id() {
 }
 
 function get_league_users($group_id) {
-
+    
     global $conn;
     $result = mysqli_query($conn, "select user_id from league_members where league_id = $group_id") or mysqli_error($conn);
     if (mysqli_num_rows($result) > 0) {
@@ -59,9 +58,10 @@ function send_league_media($group_id, $sender_id, $msg, $media_type) {
 }
 
 function get_topichat_users($group_id) {
+    echo "called";
     global $conn;
     $result = mysqli_query($conn, "select user_id from topic_group_user where topic_id = $group_id");
-
+    
     if (mysqli_num_rows($result) > 0) {
         $arr = array();
         while ($row = mysqli_fetch_assoc($result)) {
@@ -75,25 +75,29 @@ function get_topichat_users($group_id) {
 }
 
 function send_topic_msg($group_id, $sender_id, $msg) {
+    echo "in message function";
     global $conn;
-    $query = "insert into topic_group_chat value(NULL,$group_id,$sender_id,'" . $msg . "',NULL,NULL,NULL)";
-
+    $query = "insert into topic_group_chat value(NULL,$group_id,$sender_id,'" . $msg . "',NULL,NULL,".time().")";
+    
     if (mysqli_query($conn, $query)) {
+        echo "record inserted";
         return true;
     }
+    echo mysqli_error($conn);
+    echo "return false";
     return false;
 }
 
 function send_topic_media($group_id, $sender_id, $msg, $media_type) {
     global $conn;
     if ($media_type == 'links') {
-        $query = "insert into topic_group_chat value(NULL,$group_id,$sender_id,'','" . mysqli_real_escape_string($conn, $msg) . "','" . $media_type . "',NULL)";
+        $query = "insert into topic_group_chat value(NULL,$group_id,$sender_id,'','" . mysqli_real_escape_string($conn, $msg) . "','" . $media_type . "',".time().")";
         if (mysqli_query($conn, $query)) {
             return true;
         }
     } else {
         foreach ($msg as $media) {
-            $query = "insert into topic_group_chat value(NULL,$group_id,$sender_id,'','" . $media->media . "','" . $media_type . "',NULL)";
+            $query = "insert into topic_group_chat value(NULL,$group_id,$sender_id,'','" . $media->media . "','" . $media_type . "',".time().")";
             if (mysqli_query($conn, $query)) {
                 return true;
             }
