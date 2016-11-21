59e2726edad67d84b5ca5d9284df51db73ae1649
Hiren <hda@narola.email>
2016-11-21 14:09:52 +0530

    youtube video changes

--- public/chat/server.php
+++ public/chat/server.php
@@ -67,6 +67,7 @@ function wsOnMessage($clientID, $message, $messageLength, $binary) {
                         $send_object['media'] = (isset($message->media)?($message->media=='links')?$message->message :$message->message[0]->media:NULL);
                         $send_object['chat_id'] = $chat_id;
                         $send_object['media_type'] = (isset($message->media)?$message->media:NULL);
+                        $send_object['youtube_video'] = (isset($message->youtube_video) ? $message->youtube_video : NULL);
                         $send_object['group'] = get_topic_name($message->group_id);
                         foreach ($Server->wsClients as $id => $client) {
                             if ($id != $clientID && in_array($Server->wsClients[$id]['user_data']->id, $user_ids) && isset($Server->wsClients[$id]['room_id']) && $Server->wsClients[$id]['room_id'] == $message->group_id && $Server->wsClients[$id]['room_type'] == $message->type) {
