4eaec3a0fbc79f352fbbb7430d4ecd2bc8c2a70f
Hiren <hda@narola.email>
2016-11-21 18:26:07 +0530

    changes

--- public/chat/db.php
+++ public/chat/db.php
@@ -94,9 +94,12 @@ function send_topic_media($group_id, $sender_id, $msg, $media_type, $youtube_vid
         {
             $query = "insert into topic_group_chat value(NULL,$group_id,$sender_id,'','" . mysqli_real_escape_string($conn, $msg) . "','" . $media_type . "',NULL,'" . date('Y-m-d H:i:s') . "')";
         }
-        echo "query = ".$query;
         if (mysqli_query($conn, $query)) {
             return true;
+        }
+        else
+        {
+            mysqli_error($conn);
         }
     } else {
         foreach ($msg as $media) {
