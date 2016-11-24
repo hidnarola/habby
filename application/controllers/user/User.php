b3e90f84efd319714dd7b10216d0459aafb7dd06
Hemadri <hpa@narola.email>
2016-11-24 09:46:47 +0530

    no message

--- application/controllers/user/User.php
+++ application/controllers/user/User.php
@@ -283,8 +283,7 @@ class User extends CI_Controller {
 
     public function facebook_callback() {
         $user_detail = $this->facebook->get_user();
-        echo phpinfo();
-        exit;
+        pr($this->facebook);
         pr($user_detail, 1);
         $sess_user_data = $this->session->userdata('user');
         if (!empty($user_detail)) {
