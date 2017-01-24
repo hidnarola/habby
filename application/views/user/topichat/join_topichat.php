b69400279226f95c2a410c49d4015cd96382466e
Hiren <hda@narola.email>
2017-01-24 17:44:32 +0530

    no message

--- application/views/user/topichat/join_topichat.php
+++ application/views/user/topichat/join_topichat.php
@@ -803,12 +803,14 @@ $myuserdata = array(
                 $(this).parent().parent().removeClass('mini').addClass('normal');
                 $('.panel.panel-chat > .panel-body').animate({height: "250px"}, 500).show();
                 $('.panel.panel-chat > .panel-footer').animate({height: "75px"}, 500).show();
+                $(".panel-body").scrollTop(2500);
             }
             else
             {
                 $(this).parent().parent().removeClass('normal').addClass('mini');
                 $('.panel.panel-chat > .panel-body').animate({height: "0"}, 500);
                 $('.panel.panel-chat > .panel-footer').animate({height: "0"}, 500);
+                
                 setTimeout(function() {
                     $('.panel.panel-chat > .panel-body').hide();
                     $('.panel.panel-chat > .panel-footer').hide();
