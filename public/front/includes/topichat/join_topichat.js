32dd99e5cbf804d31ff677749b1c163c696d8212
Hiren <hda@narola.email>
2017-02-07 15:27:22 +0530

    no message

--- public/front/includes/topichat/join_topichat.js
+++ public/front/includes/topichat/join_topichat.js
@@ -938,7 +938,6 @@ $(document).ready(function () {
                  $('[data-chat_id="'+userdata.chat_id+'"]').each(function(){
                     $(this).find('.watching_count').html(parseInt($(this).find('.watching_count').html()) + 1);
                     $(this).find('.total_views_list').find('ul').append('<li data-watching_user_id="'+userdata.user_id+'"><a href="javascript:void(0);" title="'+userdata.user+'"><img class="user_chat_thumb" src="'+DEFAULT_PROFILE_IMAGE_PATH + "/" + userdata.user_image+'" title="'+userdata.user+'" /></li>');
-                    $(this).find('.user_post_image_right').append('<li data-watching_user_id="'+userdata.user_id+'"><a href="javascript:void(0);" title="'+userdata.user+'"><img class="user_chat_thumb" src="'+DEFAULT_PROFILE_IMAGE_PATH + "/" + userdata.user_image+'" title="'+userdata.user+'" /></li>');
                 });
             }
             else if(userdata.media_type == "post_close_view"){
