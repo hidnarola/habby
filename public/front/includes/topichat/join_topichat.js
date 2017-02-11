f0a00e165b3f9d51b4dfd313de1d0b6e949901c8
Hiren <hda@narola.email>
2017-02-11 12:39:55 +0530

    no message

--- public/front/includes/topichat/join_topichat.js
+++ public/front/includes/topichat/join_topichat.js
@@ -521,6 +521,7 @@ $(document).ready(function () {
                     reader.onloadend = function () { // set image data as background of div
                         file_flag = 1;
                         uploading_file  = !!this.files ? this.files : [];
+                        console.log("upload file = ",uploading_file);
                         // $('#imagePreview').addClass('imagePreview');
                         $('.upload_image_wrapper').show();
                         $('.message').hide();
